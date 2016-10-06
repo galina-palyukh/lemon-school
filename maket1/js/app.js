9916560a4293ad0d2f35a2630ca1f6bd3033430c
galina <g.paluykh>
2016-10-06 18:13:32 +0300

    slider and breadcrumbs

---
+++ maket1/js/app.js
@@ -0,0 +1,17 @@
+$('#slider').slick({
+	slidesToShow: 1,
+	slidesToScroll: 1,
+	dots: true,
+	autoplay: true,
+  	autoplaySpeed: 2000
+  	// fade:true
+	// responsive: [
+	// 	{	
+	// 		breakpoint: 1024,
+	// 		settings: {
+	// 			slidesToShow: 2
+	// 		}
+	// 	}
+
+	// ]
+});
