dfa5cd248dc8a738805a3cc55f503cac2b2a7773
galina <g.paluykh>
2016-10-10 22:25:28 +0300

    carousel

--- maket1/js/app.js
+++ maket1/js/app.js
@@ -1,27 +1,30 @@
-$('#slider').slick({
+$('#wrapper-carousel').slick({
 	slidesToShow: 1,
 	slidesToScroll: 1,
 	dots: true,
-	autoplay: true,
-  	autoplaySpeed: 2000
-  	// fade:true
-	// responsive: [
-	// 	{	
-	// 		breakpoint: 1024,
-	// 		settings: {
-	// 			slidesToShow: 2
-	// 		}
-	// 	}
+	autoplay: false,
+	arrows:false,
+  	autoplaySpeed: 2000,
+  	draggable:false,
+	responsive: [
+		{	
+			breakpoint: 600,
+			settings: {
+				slidesToShow: 1
+			}
+		}
 
-	// ]
+	]
 });
+
+
 $(".burger").on('click',function(){
 		$(this).toggleClass('active');
 		$('.mobile-menu').slideToggle(400);
 	});
 
 $(window).on('resize', function(){
-		if ($(window).width() > 550) {
+		if ($(window).width() > 650) {
 			$('.mobile-menu').css('display', 'none');
 			$('.burger').removeClass('active');
 		}
